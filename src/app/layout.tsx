83b59c114cc5842e1744b59a08f23cff368b706c
vicvinc <1285044294@qq.com>
2024-01-09 22:05:35 +0800

    [A] add vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script'
 import { Inter } from 'next/font/google'
 import { Toaster } from "@/components/ui/toaster"
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import './globals.css'
 
@@ -30,6 +31,7 @@ export default function RootLayout({
                 <ReactQueryProvider>{children}</ReactQueryProvider>
                 <Toaster />
                 <Analytics />
+                <SpeedInsights />
                 {
                     gAnalysis ? (
                         <>
