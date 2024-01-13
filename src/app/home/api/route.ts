92c92cf561f0355e459d2cead22f5e8ec2949508
vicvinc <1285044294@qq.com>
2024-01-14 01:03:03 +0800

    [F] server data

--- src/app/home/api/route.ts
+++ src/app/home/api/route.ts
@@ -5,10 +5,14 @@ export async function GET(request: Request) {
     const p = searchParams.get('page')
     const ps = searchParams.get('page_size')
     const apiUrl = process.env.API_URL
-    const res = await fetch(`${apiUrl}/color-sets?page=${p}&page_size=${ps}`, {
-        method: "GET",
-    })
-    const page = await res.json()
-    console.log('color set page', page)
-    return Response.json({ page })
+    try {
+        const res = await fetch(`${apiUrl}/color-sets?page=${p}&page_size=${ps}`, {
+            method: "GET",
+        })
+        const page = await res.json()
+        console.log('color set page', page)
+        return Response.json({ page })
+    } catch (error) {
+        return Response.json({ page: [] })
+    }
 }
