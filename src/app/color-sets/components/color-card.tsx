93fa145251e04007d122ab2e8125072bf0029895
vicvinc <1285044294@qq.com>
2024-03-03 22:24:45 +0800

    [U] color set

--- src/app/color-sets/components/color-card.tsx
+++ src/app/color-sets/components/color-card.tsx
@@ -1,16 +1,27 @@
 
 import "./color-card.css";
+
+// todo: formate
+const hsvFormat = (color: any) => {
+
+}
+
 export default function ColorCard({ color }: { color: any }) {
     return (
         <div
-            className="w-full h-16 border border-dotted px-4 py-2
+            className="w-full h-32 border border-dotted px-4 py-2
                 card-item
             "
             style={{
                 background: `${color.hex}`
             }}
         >
-            {color.hex}
+            <p>
+                {color.hex}
+            </p>
+            <p>
+                {color.alpha}
+            </p>
         </div>
     )
 }
