3ecc3a87184b9879123cc3b0b9d26d42165f3aa5
vicvinc <1285044294@qq.com>
2024-03-17 23:50:55 +0800

    [A] add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const siteUrl = process.env.SERVER_HOST
+    return [
+        {
+            url: `${siteUrl}`,
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.1,
+        },
+        {
+            url: `${siteUrl}/explore`,
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.4,
+        },
+        {
+            url: `${siteUrl}/color-sets`,
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1,
+        },
+    ]
+}
