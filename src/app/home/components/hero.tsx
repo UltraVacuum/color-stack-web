6cfa86308c6d57047221ae449a9b5f8fcd3aecd3
vicvinc <1285044294@qq.com>
2024-03-25 23:21:20 +0800

    [U] up

--- src/app/home/components/hero.tsx
+++ src/app/home/components/hero.tsx
@@ -51,41 +51,35 @@ export default function Hero({
     children: React.ReactNode
 }) {
     return (
-        <div className='relative h-screen bg-slate-100'>
-            {/* <div
-                className='w-full h-full absolute top-0 left-0 bottom-0 right-0 -z-50
-                opacity-10 bg-gray-200'
-            /> */}
-            <TopPolygon />
+        <div className='relative h-screen md:flex md:items-center bg-slate-100'>
             <BottomPolygon />
-            <div className='md:flex md:items-center h-full'>
-                <div className='container w-full h-full md:w-3/5 mx-auto z-10'>
-                    <div className='text-black text-center rounded-xl px-16 py-16 mt-40'>
-                        <h1 className='font-sans font-bold tracking-tighter text-6xl slogan'>
-                            Design <span className='text-green-400'>Color </span>
-                            in one <span className='text-sky-400'>Stack.</span>
-                        </h1>
-                        <div
-                            className='w-full p-16 bg-center bg-no-repeat bg-cover mt-16 rounded-sm'
-                            style={{
-                                backgroundImage: `url(${HeroImage.src})`
-                            }}
-                        >
-                            <div className='w-4/5 my-8 px-8 py-8 mx-auto bg-lime-400 bg-opacity-80 rounded-sm'>
-                                <p className='text-2xl text-white font-medium opacity-100'>
-                                    Get inspired by theme colors from all world most popular pages.
-                                </p>
-                            </div>
-                            <Link
-                                href="/explore"
-                                className="bg-green-400 text-2xl text-white font-mono font-bold px-4 py-2 rounded-sm">
-                                Explore
-                            </Link>
-                        </div>
+            <div className='container w-full px-16 mx-auto z-10'>
+                <div className='text-black text-center rounded-xl p-16'>
+                    <h1 className='font-sans font-bold tracking-tighter text-6xl slogan'>
+                        Design <span className='text-green-400'>Color </span>
+                        in one <span className='text-sky-400'>Stack.</span>
+                    </h1>
+                    <div
+                        className='w-full h-96 bg-center bg-no-repeat bg-cover mt-16 rounded-t-sm'
+                        style={{
+                            backgroundImage: `url(${HeroImage.src})`
+                        }}
+                    >
+                    </div>
+                    <div className="flex items-center px-8 py-4 mx-auto bg-lime-400 bg-opacity-90 rounded-b-sm">
+                        <p className='text-2xl text-black font-medium opacity-100'>
+                            Get inspired by theme colors from world most popular pages.
+                        </p>
+                        <Link
+                            href="/explore"
+                            className="bg-green-400 text-2xl text-white font-mono font-bold px-8 py-4 rounded-sm">
+                            Explore
+                        </Link>
                     </div>
                 </div>
-                {children}
             </div>
+            {children}
+            <TopPolygon />
         </div >
     )
 }
